fac58d27fda09062971df0cd767b132506a6b56e
Jyotirmayee Sahoo <jyotirmayeesahoo082@gmail.com>
2026-04-08 09:35:20 +0530

    innitial commit

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Room } from "../types/room";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/rooms",
+  baseURL: "http://localhost:8000/api/rooms",
 });
 
 // Get all rooms
